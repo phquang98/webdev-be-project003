3017700f2f33bd0caedd03c77a751f38c22a33d9
phquang98 <53476449+phquang98@users.noreply.github.com>
2023-04-21 09:37:55 +0700

    feat: add more services + add a new proj

--- simple-api-program/Program.cs
+++ simple-api-program/Program.cs
@@ -1,22 +1,87 @@
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using simple_api_program.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var app = builder.Build();
 
 // --- DI
 
 // --- Services
 
+// Services must be registered to be used later in app
+//builder.Services.AddAuthorization();
+
+//builder.Services.AddMvc();  // not needed as using endpoints routing already in minimal API, also deprecated
+//builder.Services.AddControllers();    // allows ctrl to access services have been registered in the app DI
+//example, allow this to happen:   private readonly IVatTuDB _iVatTuDB;public VatTuController(IVatTuDB iVatTuDB){_iVatTuDB = iVatTuDB;}
+
+//builder.Services.AddCors(opts =>
+//{
+
+//});
+
+builder.Services.AddEndpointsApiExplorer(); // required if use minimal API
+builder.Services.AddSwaggerGen(opts =>
+{
+    opts.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Simple App",
+            Description = "An ASP.NET Core Minimal API for reference",
+            License = new OpenApiLicense
+            {
+                Name = "Example License",
+                Url = new Uri("https://creativecommons.org/licenses/by-sa/4.0/")
+            }
+        }
+    );
+    opts.SwaggerDoc(
+        "v3",
+        new OpenApiInfo { Version = "v2", Title = "Simple App V2 Newer and Better", }
+    );
+}); // from Swashbuckle.AspNetCore
+
+builder.Services.AddHealthChecks();
+
 // --- Configs
+
+
+
+var app = builder.Build();
+
+// --- Middlewares
+//app.UseRouting(); // matching incoming reqs to the corresponding endpoint, always top, default in minimal API
+//app.UseMvc(); // not needed as using endpoints routing already in minimal API, also deprecated
+app.UseCors();  // placed after UseRouting, but before UseAuthorization and MapControllers
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllers();
+//});   // after matching, send incoming reqs to appropriate endpoint hdlr, redudance in minimal API as already directly send reqs to designated endpoints under MapGet, ...
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // generate a JSON file, typically at swagger/v1/swagger.json
+    app.UseSwaggerUI(swaggerUIOpts =>
+    {
+        swaggerUIOpts.SwaggerEndpoint("/swagger/v1/swagger.json", "Newer API I choose you!"); // config to choose what version of Swagger JSON
+    }); // generate swagger/index.html
+}
+app.MapHealthChecks("/healthz").RequireHost("*:9002");
+
+//app.UseAuthorization();
 
 // --- Minimal API
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/healthcheck", () => MiniAPI.HealthChecking());
+app.MapGet("/customhealthcheck", () => MiniAPI.HealthChecking());
+
+//app.MapGet("/special-healthcheck", () => MiniAPI.HealthChecking()).RequireAuthorization();
 app.MapGet(
     "/test/{jsonPlaceHolderIdHere}",
     (int jsonPlaceHolderIdHere) => MiniAPI.GetOneUserById(jsonPlaceHolderIdHere)
 );
+
+// --- Start
 
 app.Run();
